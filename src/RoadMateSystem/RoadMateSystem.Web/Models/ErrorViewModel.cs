e8710d7ef066eedb684a310d9978470555b8dbea
Teodor Golisharski <t.golisharski@abv.bg>
2023-07-20 19:46:25 +0300

    Project build

---
+++ src/RoadMateSystem/RoadMateSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoadMateSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
